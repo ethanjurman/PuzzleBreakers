b099e26c9282cedea128c6e37ae158c5693dba7f
Ethan Hale Jurman <ehj2229@rit.edu>
2013-02-25 18:26:16 -0500

    removed unused '|| upcase' lines

--- anagramSolver.rb
+++ anagramSolver.rb
@@ -1,8 +1,7 @@
 # ANAGRAM SOLVER by ETHAN H. JURMAN
 charsHash = Hash.new(0) # build hash, with keys = char; values = number of chars avaliable in the chars string
 string = ARGV[0] || gets().chop! # remove newline
-string = string.upcase || string # some input strings are lower case
-string = string.split("").sort() # split it! (and sort it)
+string = string.upcase.split("").sort() # split it! (and sort it)
 string.each do |char| #building hash
   charsHash[char] = charsHash[char] + 1
 end
@@ -15,8 +14,7 @@ regex = Regexp.new("^#{parseString}\\z") #finalize regex (\\A and \\z dictate th
 filename = 'dictionary.txt' # use a dictionary file to find words
 words = [] # our words array, print this later to find results
 File.open(filename, 'r').each_line do |line| 
-  line = line.upcase || line # some dictionarys use caps, this will caps only if the line isn't already
-  line = line.split("\n")[0] #don't need the new line character
+  line = line.upcase.split("\n")[0] #don't need the new line character
   if line.split("").sort().join("").scan(regex).join("") == line.split("").sort().join("") #if the line (sorted) matches a regex parsing
     words << line # add the word to our words array
   end
---
+++ anagramTests.rb
@@ -0,0 +1,4 @@
+require './anagramSolver.rb'
+require 'test/unit'
+
+class anagramTest < Test::Unit::TestCase
