491a6273eaf7f73a39c316f5ba319c179e0cc9ca
Ethan Hale Jurman <ehj2229@rit.edu>
2013-02-25 20:34:31 -0500

    made some edits to shorten the code, also implements match(regex) instead of scan(regex)

--- anagramSolver.rb
+++ anagramSolver.rb
@@ -1,21 +1,17 @@
 # ANAGRAM SOLVER by ETHAN H. JURMAN
 charsHash = Hash.new(0) # build hash, with keys = char; values = number of chars avaliable in the chars string
-string = ARGV[0] || gets().chop! # remove newline
-string = string.upcase.split("").sort() # split it! (and sort it)
+string = (ARGV[0] || gets().chop!).upcase.split("").sort() # split it! (and sort it)
 string.each do |char| #building hash
-  charsHash[char] = charsHash[char] + 1
-end
+  charsHash[char] = charsHash[char] + 1 end
 parseString = ""
 charsHash.each do |char, num| # [char]{0,value} limits char usages from 0 to value uses
-  parseString << "[#{char}]{0,#{num}}"
-end
+  parseString << "[#{char}]{0,#{num}}" end
 regex = Regexp.new("^#{parseString}\\z") #finalize regex (\\A and \\z dictate that there will only be one regex per line)
-# puts regex
 filename = 'dictionary.txt' # use a dictionary file to find words
 words = [] # our words array, print this later to find results
 File.open(filename, 'r').each_line do |line| 
   line = line.upcase.split("\n")[0] #don't need the new line character
-  if line.split("").sort().join("").scan(regex).join("") == line.split("").sort().join("") #if the line (sorted) matches a regex parsing
+  if line.split("").sort().join("").match(regex) #if the line (sorted) matches a regex parsing
     words << line # add the word to our words array
   end
 end
